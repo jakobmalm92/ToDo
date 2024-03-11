a6129f6779d34c8609fe144067264fb26aed341d
jakob.malm <malm92_rhotmail.com>
2024-03-11 21:39:20 +0100

    skapat upp min todoPage med olika funktioner

--- laborationtodo/src/assets/components/TodoPage.jsx
+++ laborationtodo/src/assets/components/TodoPage.jsx
@@ -1,1 +1,33 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+function TodoPage() {
+
+    const location = useLocation();
+    const {name} = location.state
+
+//Lägga till en uppgift
+    const addTodo = () => {
+
+    }
+
+
+    //Ta bort en uppgift
+    const removeTodo = () => {
+
+    }
+
+
+    // Markera om uppgiften är klar eller inte
+    const todoCompleted = () => {
+
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TodoPage
 
